e0da957b8ed483979e53464cd1a06358c8db516f
Kaufman <alexkaufman06@gmail.com>
2019-08-02 07:10:22 -0700

    Update mailer and README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Daily Updater
+# Daily Updater [![Build Status](https://travis-ci.com/alexkaufman06/dailyUpdater.svg?branch=master)](https://travis-ci.com/alexkaufman06/dailyUpdater)
 
-[![Build Status](https://travis-ci.com/alexkaufman06/dailyUpdater.svg?branch=master)](https://travis-ci.com/alexkaufman06/dailyUpdater)
+The goal of this repository is to gather data every morning and email to interested users. Some examples of data that might be useful include:
+
+* Weather
+  * Local and elsewhere
+* Stock Market
+* CraigsList
+* Traffic
--- mailer.js
+++ mailer.js
@@ -1,10 +1,24 @@
 var nodemailer = require('nodemailer');
 
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASSWORD
+//   }
+// });
+
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
+    type: 'OAuth2',
     user: process.env.EMAIL,
-    pass: process.env.PASSWORD
+    clientId: '000000000000-xxx0.apps.googleusercontent.com',
+    clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
+    refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
+    accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
   }
 });
 
@@ -14,20 +28,6 @@ var mailOptions = {
   subject: 'Sending Email using Node.js',
   text: 'That was easy!'
 };
-
-var transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-      type: 'OAuth2',
-      user: 'user@example.com',
-      clientId: '000000000000-xxx0.apps.googleusercontent.com',
-      clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
-      refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-      accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
-  }
-});
 
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
