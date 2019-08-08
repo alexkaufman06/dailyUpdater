178eac27f8cf0d0b8e7981c8c2c5bf1bff4a0d3f
Alex Kaufman <alexkaufman06@gmail.com>
2019-08-07 18:43:58 -0700

    Another attempt at sending an email

--- mailer.js
+++ mailer.js
@@ -1,38 +1,80 @@
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD
-//   }
-// });
 
-var transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID, // ClientID
+  process.env.CLIENT_SECRET, // Client Secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+     refresh_token: process.env.REFRESH_TOKEN
+});
+
+const accessToken = oauth2Client.getAccessToken()
+
+const smtpTransport = nodemailer.createTransport({
+  service: "gmail",
   auth: {
     type: 'OAuth2',
     user: process.env.EMAIL,
     clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-    // refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-    // accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
-  }
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN
+   }
 });
 
-var mailOptions = {
-  from: process.env.EMAIL,
-  to: process.env.EMAIL,
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+const mailOptions = {
+     from: process.env.EMAIL,
+     to: process.env.EMAIL,
+     subject: "Node.js Email with Secure OAuth",
+     generateTextFromHTML: true,
+     html: "<b>test</b>"
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+smtpTransport.sendMail(mailOptions, (error, response) => {
+     error ? console.log(error) : console.log(response);
+     smtpTransport.close();
 });
+
+
+// // var transporter = nodemailer.createTransport({
+// //   service: 'gmail',
+// //   auth: {
+// //     user: process.env.EMAIL,
+// //     pass: process.env.PASSWORD
+// //   }
+// // });
+
+// var transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     type: 'OAuth2',
+//     user: process.env['EMAIL'],
+//     clientId: process.env['CLIENT_ID'],
+//     clientSecret: process.env['CLIENT_SECRET'],
+//     refreshToken: process.env['REFRESH_TOKEN'],
+//     accessToken: process.env['ACCESS_TOKEN']
+//   }
+// });
+
+// var mailOptions = {
+//   from: process.env['EMAIL'],
+//   to: process.env['EMAIL'],
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
